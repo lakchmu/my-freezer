26b3beb0144f2126498e22d3049a3670c601662f
Daria Kurmaeva <lakchmu@gmail.com>
2023-06-04 15:15:21 +0400

    MF:18 Fix setup app

--- src/rest/axios.ts
+++ src/rest/axios.ts
@@ -2,9 +2,10 @@ import axios from 'axios';
 
 import CookieService from '../utils/token.service';
 
-const baseURL = `${process.env.BASE_URL}/api`;
-
 const getHttpClient = async ({ headers }: any) => {
+  const baseURL = `${process.env.BASE_URL}/api`;
+  console.log('API baseURL: ', baseURL);
+
   const bearer = await CookieService.getUserToken();
 
   return axios.create({
